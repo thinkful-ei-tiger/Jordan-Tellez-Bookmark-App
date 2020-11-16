e35da056439be16858432ae4fc0e9f04002664b6
Jordan Tellez <jordantellez@jordans-MacBook-Pro.local>
2020-11-16 15:15:53 -0800

    final version

---
+++ src/api.js
@@ -0,0 +1,62 @@
+const BASE_URL =
+  "https://thinkful-list-api.herokuapp.com/jordan-tellez/bookmarks";
+
+function listApiFetch(...args) {
+  let error;
+  return fetch(...args)
+    .then((res) => {
+      if (!res.ok) {
+        // Create an error!
+        error = {
+          code: res.status,
+        };
+      }
+
+      // In either case, parse the JSON stream:
+      return res.json();
+    })
+    .then((data) => {
+      // If error was flagged, reject the Promise with the error object
+      if (error) {
+        error.message = data.message;
+        return Promise.reject(error);
+      }
+
+      // Otherwise give back the data as resolved Promise
+      return data;
+    });
+}
+
+const getBookmarks = function () {
+  return fetch(`${BASE_URL}`);
+};
+
+const createBookmark = function (obj) {
+  const newBookmark = obj;
+  console.log("newBookmark:", newBookmark);
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: newBookmark,
+  };
+
+  return listApiFetch(`${BASE_URL}`,options);
+};
+
+const deleteBookmark = function (objId) {
+  const options = {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json",
+    },
+  };
+  return listApiFetch(`${BASE_URL}/${objId}`,options);
+};
+
+export default {
+  getBookmarks,
+  createBookmark,
+  deleteBookmark,
+};
